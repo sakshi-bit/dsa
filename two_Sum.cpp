6c3d8fafe468021603836977efbb310d35572182
sakshi_bit <17june2001@gmail.com>
2022-06-23 10:32:29 +0530

    added

--- two_Sum.cpp
+++ two_Sum.cpp
@@ -2,16 +2,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
-            }  
+    vector<int> ans;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            if(mpp.find(target-nums[i])!=mpp.end()){
+                ans.push_back(mpp[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+
+            mpp[nums[i]] = i;
         }
-        return {-1,-1};
         
+        return ans;
     }
 int main() {
     int n,target;
